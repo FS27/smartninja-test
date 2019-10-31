3d8d1f4bb24ac8d2303a13b5cd7905310ea2755c
FS27 <fla_silva2006@hotmail.com>
2019-10-31 20:00:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
 
-    return render_template("index.html")
+    return render_template("index.html", some_text=some_text, current_year=current_year)
 
 
 @app.route("/about.html")
